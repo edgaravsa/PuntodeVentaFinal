de28853727df67a2a48812f4bb10ceb9ff3aae3c
edgaravsa <edgar.avsa@gmail.com>
2025-10-24 12:59:27 -0600

    Add EmployeeNumber and EmployeeRole to Employee class

--- POSv2.Domain/Entities/Employee.cs
+++ POSv2.Domain/Entities/Employee.cs
@@ -3,10 +3,18 @@ namespace POSv2.Domain.Entities
     public class Employee
     {
         public Guid Id { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string Name { get; set; }
         public string Username { get; set; } // Login único
-        public string FullName { get; set; }
         public string PasswordHash { get; set; } // Hasheada
-        public string Role { get; set; } // "Cajero", "Gerente", "Administrador"
+        public EmployeeRole Role { get; set; } // Enum
         public bool IsActive { get; set; }
+    }
+
+    public enum EmployeeRole
+    {
+        Cashier,
+        Manager,
+        Administrator
     }
 }
