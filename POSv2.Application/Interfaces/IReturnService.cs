94961155c66ca4cd8c53025b2d62532ca50766d6
edgaravsa <edgar.avsa@gmail.com>
2025-10-24 13:10:45 -0600

    Fix namespace declaration for IReturnService

--- POSv2.Application/Interfaces/IReturnService.cs
+++ POSv2.Application/Interfaces/IReturnService.cs
@@ -1,6 +1,9 @@
-public interface IReturnService
+namespace POSv2.Application.Interfaces
 {
-    Task CancelOrderAsync(Guid orderId, string reason, string employeeId);
-    Task ReturnItemsAsync(Guid orderId, List<ReturnItem> items, string reason, string employeeId);
-    Task<List<ReturnOrCancellation>> GetReturnsAndCancellationsAsync(DateTime? from = null, DateTime? to = null);
+    public interface IReturnService
+    {
+        Task CancelOrderAsync(Guid orderId, string reason, string employeeId);
+        Task ReturnItemsAsync(Guid orderId, List<ReturnItem> items, string reason, string employeeId);
+        Task<List<ReturnOrCancellation>> GetReturnsAndCancellationsAsync(DateTime? from = null, DateTime? to = null);
+    }
 }
