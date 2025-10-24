adfc9fcda07eff6153786b59c2cf5386fc0a6005
edgaravsa <edgar.avsa@gmail.com>
2025-10-24 13:20:51 -0600

    Fix namespace declaration for AuditLog class

--- POSv2.Domain/Entities/AuditLog.cs
+++ POSv2.Domain/Entities/AuditLog.cs
@@ -1,9 +1,12 @@
-public class AuditLog
+namespace POSv2.Domain.Entities
 {
-    public Guid Id { get; set; }
-    public string UserId { get; set; }
-    public string Module { get; set; }
-    public string Action { get; set; }
-    public string Details { get; set; }
-    public DateTime Timestamp { get; set; }
+    public class AuditLog
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string Module { get; set; }
+        public string Action { get; set; }
+        public string Details { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
 }
